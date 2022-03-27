54023e1acb4a276f894a4056082b1cd0aab3668b
Ryan Blake <xbungalowx@gmail.com>
2022-03-26 19:10:10 -0500

    init commit

---
+++ src/Mole.js
@@ -0,0 +1,23 @@
+import {useEffect} from 'react'
+import mole from './mole.png'
+
+function Mole(props){
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+    return(
+        <div>
+            <img style={{'width': '30vw'}} 
+                src={mole}
+                alt='mole'
+                onClick={props.handleClick}
+            />
+        </div>
+    )
+}
+
+export default Mole
